4ba894a3c0171ec5c0dbb1aa9d415148461eb4e4
Adam Howes <adamhowes@outlook.com>
2020-07-14 14:22:43 +0100

    plot_matrix is more intuitive than matrix_plot

--- R/plotting.R
+++ R/plotting.R
@@ -4,13 +4,17 @@
 #' @return A \code{ggplot2} plot.
 #' @examples
 #' M <- matrix(1:9, nrow = 3, ncol = 3)
-#' matrix_plot(M)
+#' plot_matrix(M)
 #'
-#' matrix_plot(border_precision(mw))
-matrix_plot <- function(M){
+#' plot_matrix(border_precision(mw))
+plot_matrix <- function(M){
   M <- t(apply(M, 2, rev)) # Undo 90 degree CC rotation
   ggplot2::ggplot(reshape2::melt(M), aes(x = Var1, y = Var2, fill = value)) +
     labs(title = paste0("Visualise matrix: ", deparse(substitute(M))),
          x = "", y = "", fill = "Value") +
-    geom_tile()
+    geom_tile() + 
+    theme(axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())
 }
