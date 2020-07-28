71322312c11fc9594f23bd9b776ad85a4a7c1e1c
Adam Howes <adamhowes@outlook.com>
2020-07-28 16:49:43 +0100

    Add scale_gmrf_precision from naomi, replace \code with `

--- R/covariance_structures.R
+++ R/covariance_structures.R
@@ -1,6 +1,6 @@
-#' Create neighbourlood list from from \code{sf} object.
+#' Create neighbourlood list from from `sf` object.
 #'
-#' Wrapper function for \code{spdep::poly2nb}.
+#' Wrapper function for `spdep::poly2nb`.
 #'
 #' @param sf A simple features object with some geometry.
 #' @return A neighbourhood list object.
@@ -14,10 +14,10 @@ neighbours <- function(sf){
 #'
 #' @param nb A neighbourhood list object.
 #' @return A list containing
-#' * \code{n} The number of nodes in the graph
-#' * \code{n_edges} To-do
-#' * \code{node1} To-do
-#' * \code{node2} To-do
+#' * `n` The number of nodes in the graph
+#' * `n_edges` To-do
+#' * `node1` To-do
+#' * `node2` To-do
 #' @source From \href{https://github.com/stan-dev/example-models/blob/master/knitr/car-iar-poisson/nb_data_funs.R}{code} by Mitzi Morris.
 #' @examples
 #' nb <- neighbours(mw)
@@ -49,7 +49,7 @@ nb_to_graph <- function(nb){
   return(list("n" = n, "n_edges" = n_edges, "node1" = node1, "node2" = node2))
 }
 
-#' Create ICAR precision matrix from \code{nb}
+#' Create ICAR precision matrix from `nb`
 #'
 #' This function creates the Besag model ICAR precision matrix \eqn{Q}.
 #' \eqn{Q} has the entries \eqn{Q_{ij} = -1} if \eqn{i \sim j} (where
@@ -57,7 +57,7 @@ nb_to_graph <- function(nb){
 #' neighbours of region \eqn{i} and \eqn{Q_{ij} = 0} otherwise.
 #'
 #' @param nb A neighbourhood list object.
-#' @return An ICAR precision matrix based upon \code{nb}.
+#' @return An ICAR precision matrix based upon `nb`.
 #' @examples
 #' nb <- neighbours(mw)
 #' nb_to_precision(nb)
@@ -71,22 +71,22 @@ nb_to_precision <- function(nb){
   return(Q)
 }
 
-#' Create object containing lengths of borders from \code{sf} object.
+#' Create object containing lengths of borders from `sf` object.
 #'
-#' The function \code{border_precision} creates a dataframe for each area of 
-#' \code{sf} containing information about the shared borders with other areas.
-#' Within this package the primary use is within \code{\link{border_precision}}.
+#' The function [`border_precision`] creates a dataframe for each area of 
+#' `sf` containing information about the shared borders with other areas.
+#' Within this package the primary use is within [`border_precision`].
 #'
 #' @param sf A simple features object with some geometry.
-#' @return A list of \code{nrow(sf)} data frames which each have columns:
-#' * \code{origin} The origin node.
-#' * \code{perimeter} The perimeter of the origin node.
-#' * \code{touching} The index of a node having shared border with the origin
+#' @return A list of `nrow(sf)` data frames which each have columns:
+#' * `origin` The origin node.
+#' * `perimeter` The perimeter of the origin node.
+#' * `touching` The index of a node having shared border with the origin
 #' node.
-#' * \code{length} The length of border between \code{origin} and
-#' \code{touching}.
-#' * \code{weight} The proportion of \code{origin}'s border which is with
-#' \code{touching}.
+#' * `length` The length of border between `origin` and
+#' `touching`.
+#' * `weight` The proportion of `origin`'s border which is with
+#' `touching`.
 #' @examples
 #' border_lengths(mw)
 border_lengths <- function(sf){
@@ -114,7 +114,7 @@ border_lengths <- function(sf){
   return(all_lengths)
 }
 
-#' Create border ICAR precision matrix from \code{sf}.
+#' Create border ICAR precision matrix from `sf`.
 #'
 #' This function creates the a weighted ICAR precision matrix \eqn{Q}.
 #' In \eqn{Q} each entry \eqn{Q_{ij}} is equal to the shared length of
@@ -123,7 +123,7 @@ border_lengths <- function(sf){
 #' of each region.
 #'
 #' @param sf A simple features object with some geometry.
-#' @return An ICAR precision matrix based upon \code{sf}.
+#' @return An ICAR precision matrix based upon `sf`.
 #' @examples
 #' border_precision(mw)
 border_precision <- function(sf){
@@ -139,58 +139,98 @@ border_precision <- function(sf){
   return(Q)
 }
 
-#' Compute scale of a precision matrix using \code{R-INLA}.
+#' Compute scale of a precision matrix using `R-INLA}.
 #' 
-#' See \code{INLA::inla.scale.model}.
+#' Here scale refers to the Riebler generalised variance (see 
+#' [`riebler_gv`]). This is computed by first inverting
+#' the precision matrix, which may first require imposing 
+#' constraints. Note that if using this function directly on
+#' `nb_to_precision(nb)` for some neighbourhood structure `nb`
+#' then it is assumed that the graph is fully connected. 
 #' 
 #' @param Q A (square, symmetric) precision matrix.
-#' @section Warning:
-#' Have not taken into account connectedness, this is a to do.
+#' @param constraint A list with arguments `A` and `e` which imposes
+#' the constraint `AQ = e`. See the `?INLA::f` argument 
+#' `extraconstr`. In the original setting the constraint is placed upon a
+#' vector rather than a precision matrix. For this reason, here `A` is a 
+#' row vector instead of a matrix.
+#' 
 #' @examples
 #' nb <- neighbours(mw)
 #' Q <- nb_to_precision(nb)
 #' get_scale(Q)
-get_scale <- function(Q){
+get_scale <- function(Q, constraint = list(A = matrix(1, 1, nrow(Q)), e = 0)){
   n <- nrow(Q)
-
   # Add jitter to the diagonal for numerical stability
   Q_prt <- Q + Matrix::Diagonal(n) * max(diag(Q)) * sqrt(.Machine$double.eps)
-
   # Inversion of sparse matrix
-  constraint <- list(A = matrix(1, 1, n, e = 0))
-  Q_inv <- as.matrix(INLA::inla.qinv(Q_prt, constr = constraint))
-  
+  Q_inv <- INLA::inla.qinv(Q_prt, constr = constraint)
   # Compute the generalised variance on the covariance matrix
-  return(riebler_gv(Q_inv))
+  return(riebler_gv(as.matrix(Q_inv)))
+}
+
+#' Scales the precision matrix of a Gaussian Markov random field.
+#' 
+#' Implements the same thing as `INLA::inla.scale.model`.
+#' 
+#' @inheritParams get_scale
+#' @param A 
+#' 
+#' @source From \href{https://github.com/mrc-ide/naomi/blob/master/R/car.R}{code} by Jeff Eaton.
+#' @examples
+#' nb <- neighbours(mw)
+#' Q <- nb_to_precision(nb)
+#' scale_gmrf_precision(Q)
+scale_gmrf_precision <- function(Q, A = matrix(1, 1, nrow(Q))){
+  
+  nb <- spdep::mat2listw(abs(Q))$neighbours
+  comp <- spdep::n.comp.nb(nb)
+  
+  for (k in seq_len(comp$nc)) {
+    idx <- which(comp$comp.id == k)
+    Qc <- Q[idx, idx, drop = FALSE]
+    
+    if (length(idx) == 1) {
+      Qc[1, 1] <- 1 # Set marginal variance for islands to be 1
+    } else {
+      Ac <- A[ , idx, drop = FALSE]
+      scaling_factor <- get_scale(Qc, constraint = list(A = Ac, e = 0))
+      Qc <- scaling_factor * Qc
+    }
+    
+    Q[idx, idx] <- Qc
+  }
+  
+  return(Q)
 }
 
 #' Compute regional covariance matrix using kernel on centroids.
 #'
 #' @param sf A simple features object with some geometry.
-#' @param kernel A kernel function, defaults to \code{matern}.
-#' @param ... Additional arguments to \code{kernel}.
-#' @return A \code{nrow(sf)} by \code{nrow(sf)} matrix.
+#' @param kernel A kernel function, defaults to `matern`.
+#' @param ... Additional arguments to `kernel`.
+#' @return A `nrow(sf)` by `nrow(sf)` matrix.
 #' @examples
 #' centroid_covariance(mw)
 centroid_covariance <- function(sf, kernel = matern, ...){
   cent <- sf::st_centroid(sf$geometry)
   dist <- sf::st_distance(cent, cent)
   cov <- apply(dist, c(1, 2), FUN = kernel, ...)
-  return(cov)
+  return(as.matrix(cov))
 }
 
 #' Compute regional covariance matrix using sample-based kernel.
 #'
-#' \code{sampling_covariance} draws \code{S} samples uniformly from inside each 
-#' region of \code{sf}. The kernel function \code{kernel} (with additional
-#' arguments \code{...}) is averaged over each pair of draws between regions
+#' `sampling_covariance` draws `S` samples uniformly from inside each 
+#' region of `sf`. The kernel function `kernel` (with additional
+#' arguments `...`) is averaged over each pair of draws between regions
 #' to produce the entries of the covariance matrix.
 #'
 #' @param sf A simple features object with some geometry.
-#' @param kernel A kernel function, defaults to \code{matern}.
-#' @param ... Additional arguments to \code{kernel}.
+#' @param kernel A kernel function, defaults to `matern`.
+#' @param ... Additional arguments to `kernel`.
 #' @param L The number of Monte Carlo samples to draw from each region.
-#' @return A \code{nrow(sf)} by \code{nrow(sf)} matrix.
+#' @return A `nrow(sf)` by `nrow(sf)` matrix.
 #' @examples
 #' sampling_covariance(mw)
 sampling_covariance <- function(sf, kernel = matern, ..., L = 100){
