c688aae9a65959e753234b31af47e1f192a368ae
Adam Howes <adamhowes@outlook.com>
2020-11-16 12:09:34 +0000

    Remove @useDynLib tag for now

--- R/roxygen_tags.R
+++ R/roxygen_tags.R
@@ -3,8 +3,6 @@
 #' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
 #' This tag will populate the namespace with compiled C++ functions upon package install.
 #'
-#' @useDynLib model1
-#'
 dummy <- function(){
   return(NULL)
 }
