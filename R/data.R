7507b32850efd21c6cfa2c208a096dc8d2d42caf
Adam Howes <adamhowes@outlook.com>
2021-01-12 21:53:02 +0000

    Running roxygenize()

--- R/data.R
+++ R/data.R
@@ -1,27 +1,4 @@
-#' HIV Survey Data.
-#'
-#' A data frame containing HIV survey data from 38 surveys across 19 African countries.
-#'
-#' @format A simple features data frame with 2435 rows and 13 variables:
-#' * `survey_id`
-#' * `gid_0`
-#' * `gid_1`
-#' * `gid_2`
-#' * `name_0`
-#' * `name_1`
-#' * `name_2`
-#' * `n_cluster`
-#' * `n_obs`
-#' * `est`
-#' * `se`
-#' * `y`
-#' * `geometry`
-#' @source Jeff Eaton
-"hiv_surveys"
-
 #' Malawi DHS 2015 Survey Data.
-#'
-#' `hiv_surveys` subsetted to just the DHS 2015 survey in Malawi.
 #'
 #' @format A simple features data frame with 28 rows and 13 variables.
 #' @source Jeff Eaton
