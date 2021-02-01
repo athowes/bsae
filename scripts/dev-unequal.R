5cd9ef7e0cfc3734de904ff3b5f69bd013484bb9
Adam Howes <adamhowes@outlook.com>
2021-02-01 09:24:40 +0000

    Data structures for unequal number of points (padded array)

---
+++ scripts/dev-unequal.R
@@ -0,0 +1,21 @@
+# Development of sampling unequal numbers of points from within each area
+
+L <- 10
+n <- nrow(mw)
+
+hexagonal <- sf::st_sample(mw, size = rep(L, n), type = "hexagonal", exact = TRUE)
+sample_index <- sf::st_intersects(mw, hexagonal)
+
+# Not all the same!
+sample_lengths <- lengths(sample_index)
+max_length <- max(sample_lengths)
+
+padded_sample_index <- lapply(sample_index, function(x) {
+  diff <- max_length - length(x)
+  append(x, values = rep(NA, diff))
+})
+
+matrix(unlist(padded_sample_index), nrow = n, ncol = max_length, byrow = TRUE)
+
+# An alternative data structure?
+group_db <- data.frame(i = unlist(sample_index), group_id = rep(seq(length(sample_index)), lengths(sample_index)))
