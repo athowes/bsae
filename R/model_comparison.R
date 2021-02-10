19c215acc14fc50b5d6ca71000659af16aad3bb2
Adam Howes <adamhowes@outlook.com>
2021-02-10 16:14:42 +0000

    bsae::crps()

--- R/model_comparison.R
+++ R/model_comparison.R
@@ -106,3 +106,13 @@ info_criteria <- function(fit){
   }
   return(ic)
 }
+
+#' Compute an approximation to the continuous ranked probability score.
+#'
+#' @param samples A collection of draws from a probability distribution.
+#' @param true_value The underlying true value that is being compared to.
+#' @return The continuous ranked probability score (CRPS)
+#' @export
+crps <- function(samples, true_value){
+  mean(abs(samples - true_value)) - 0.5 * mean(outer(samples, samples, FUN = function(x, y) abs(x - y)))
+}
