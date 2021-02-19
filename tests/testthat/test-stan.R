7beccbcb11b4a153ee8342b38a785a0408dd7706
Adam Howes <adamhowes@outlook.com>
2021-02-19 16:44:35 +0000

    capture.output of Stan call to make tests summary easier to read

--- tests/testthat/test-stan.R
+++ tests/testthat/test-stan.R
@@ -1,4 +1,4 @@
 test_that("the constant model fits using Stan", {
-  fit <- constant_stan(mw, nsim_warm = 200, nsim_iter = 400)
+  capture.output(fit <- constant_stan(mw, nsim_warm = 200, nsim_iter = 400))
   expect_s4_class(fit, "stanfit")
 })
