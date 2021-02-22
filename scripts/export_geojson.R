b63038ba137ab289aaa1ee057d0dba9e3292f110
Adam Howes <adamhowes@outlook.com>
2021-02-22 18:20:19 +0000

    Minor changes to merged pull request #1

--- scripts/export_geojson.R
+++ scripts/export_geojson.R
@@ -2,9 +2,8 @@ library(geojson)
 countries <- c("ci", "mw", "tz", "zw")
 
 for (country in countries) {
-  # load data
+  # Load data
   load(paste("data/", country, ".rda", sep = ""))
   data <- as.geojson(eval(as.symbol(country)))
-  geo_write(data, paste("data/export_data/", country, ".geojson", sep=""))
-  
+  geo_write(data, paste("data/geojson/", country, ".geojson", sep = ""))
 }
